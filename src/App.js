841f56c54160d79d354420a1ab90146ff17ba744
Jerome <ducampdorgasjerome@gmail.com>
2022-03-14 16:00:58 +0100

    Contact component creation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Contact from "./components/Contact.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center">My contacts list📱</h1>
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+function Contact() {
+  return (
+    <div>
+      <div>
+        <h3>Bob Doe</h3>
+        <h4>📞 03.49.79.52.46</h4>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+export default Contact;
